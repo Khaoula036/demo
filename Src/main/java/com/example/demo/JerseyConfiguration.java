24c4be3245e4756654e2cbff2023eee197ed9cb5
Khaoula Benaicha <khaoulabenaicha036@gmail.com>
2022-06-05 23:18:11 +0200

    modifiaction

--- Src/main/java/com/example/demo/JerseyConfiguration.java
+++ Src/main/java/com/example/demo/JerseyConfiguration.java
@@ -19,15 +19,8 @@ import javax.ws.rs.ApplicationPath;
 		register(DestinationResource.class);
 		register(CORSResponseFilter.class);
 
-		// La servlet de jersey map toutes les url /* donc impossible de servir du
-		// 
-		// 
-		// 
-		// contenu statique via la servlet par défaut de sprign boot.
-		// 
-		// On doit donc dire à jersey de quand même laisser passer la requête pour que
-		// la prochaine servlet prenne le relais.
-		// https://stackoverflow.com/questions/29658240/spring-boot-jersey-allow-jersey-to-serve-static-content/29670751#29670751
+		// La servlet de jersey map toutes les url /* donc impossible de servir du contenu statique via la servlet par défaut de sprign boot.
+		
 		property(ServletProperties.FILTER_FORWARD_ON_404, true);
 	}
 }
